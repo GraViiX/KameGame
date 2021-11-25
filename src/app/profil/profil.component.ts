3464123b6cec651dedc553e5c7b05f34dba70d5c
RasmusRBrune <rasmus14.rosenqvist@gmail.com>
2021-11-25 10:05:57 +0100

    form edit

--- src/app/profil/profil.component.ts
+++ src/app/profil/profil.component.ts
@@ -1,6 +1,6 @@
 import { visitAll } from '@angular/compiler';
 import { variable } from '@angular/compiler/src/output/output_ast';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -9,52 +9,65 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./profil.component.css']
 })
 export class ProfilComponent implements OnInit {
-
+  //#region validation messages
   public validation_messages = {
-    'username': [
+    'Username': [
       { type: 'required', message: 'Username is required' }
     ],
     'uPassword': [
       { type: 'required', message: 'Password is required' },
       { type: 'minlength', message: 'Password must be at least 8 characters long' }
     ],
-    'email': [
-      { type: 'required', message: 'Email is required' },
-      { type: 'email', message: 'Enter a valid email' }
-    ],
     'confirm_password': [
       { type: 'required', message: 'Confirm password is required' },
       { type: 'areEqual', message: 'Password mismatch' }
     ],
-    'password': [
-      { type: 'required', message: 'Password is required' },
-      { type: 'minlength', message: '' },
-      { type: 'pattern', message: 'Your password must contain at least one uppercase, one lowercase, and one number' }
+    'Email': [
+      { type: 'required', message: 'Email is required' },
+      { type: 'email', message: 'Enter a valid email' }
     ],
-    'terms': [
-      { type: 'pattern', message: 'You must accept terms and conditions' }
+    'UTLF': [
+      { type: 'required', message: 'UTLF is required' },
+      { type: 'pattern', message: 'Enter a valid  phone number' }
+    ],
+    'Type': [
+      { type: 'required', message: 'Cardtype is required' }
+    ],
+    'CardNumber': [
+      { type: 'required', message: 'Card Number is required' }
+    ],
+    'CardDate': [
+      { type: 'required', message: 'Card Date is required' }
+    ],
+    'SecurityNumber': [
+      { type: 'required', message: 'SecurityNumber is required' },
+      { type: 'pattern', message: 'Enter a valid securityNumber' }
+    ],
+    'FirstName': [
+      { type: 'required', message: 'FirstName is required' }
+    ],
+    'LastName': [
+      { type: 'required', message: 'LastName is required' }
+    ],
+    'PostCode': [
+      { type: 'required', message: 'PostCode is required' }
+    ],
+    'City': [
+      { type: 'required', message: 'City is required' }
+    ],
+    'StreetNames': [
+      { type: 'required', message: 'StreetNames is required' }
     ]
   }
+  //#endregion
 
+  //#region edit form
   EditAccountForm = new FormGroup({
     Username: new FormControl('', [Validators.required]),
     uPassword: new FormControl('', [Validators.required, Validators.minLength(8)]),
     Email: new FormControl('', [Validators.required, Validators.email]),
-    UTLF: new FormControl(0, [Validators.required, Validators.minLength(8)]),
+    UTLF: new FormControl(null, [Validators.required, Validators.pattern("[0-9]{8}")]),
     CardId: new FormControl(0),
-    Card: new FormGroup({
-      CardId: new FormControl(0),
-      CardTypeId: new FormControl(0),
-      CardType: new FormGroup({
-        CardTypeId: new FormControl(0),
-        Type: new FormControl('', Validators.required)
-      }),
-      CardNumber: new FormControl(0, Validators.required),
-      CardDate: new FormControl('', Validators.required),
-      SecurityNumber: new FormControl(0, Validators.required),
-      FirstName: new FormControl('', Validators.required),
-      LastName: new FormControl('', Validators.required)
-    }),
     Roleid: new FormControl(1),
     AddressId: new FormControl(0),
     Address: new FormGroup({
@@ -62,23 +75,40 @@ export class ProfilComponent implements OnInit {
       PostCodeId: new FormControl(null, Validators.required),
       PostCode: new FormGroup({
         PostCodeId: new FormControl(0),
-        PostCode: new FormControl(0, Validators.required),
+        PostCode: new FormControl(null, [Validators.required]),
         City: new FormControl('', Validators.required)
       }),
       StreetNames: new FormControl('', Validators.required)
     })
   });
+  //#endregion
 
+  //#region Add Card Form
+  AddCardForm = new FormGroup({
+    CardId: new FormControl(0),
+    UserId: new FormControl(0), //skal være lig med id som vi får da bruger logger ind
+    CardTypeId: new FormControl(0),
+    CardNumber: new FormControl(null, [Validators.required, Validators.pattern("[0-9]{4} [0-9]{4} [0-9]{4} [0-9]{4}")]),
+    CardDate: new FormControl('', Validators.required),
+    SecurityNumber: new FormControl(null, [Validators.required, Validators.pattern("[0-9]{3}")]),
+    FirstName: new FormControl('', Validators.required),
+    LastName: new FormControl('', Validators.required)
+  })
+  //#endregion
   constructor() { }
 
   ngOnInit(): void {
-
-
   }
 
 
   Savechange() {
     console.log(this.EditAccountForm.value);
+  }
+
+  AddCard() {
+    let firstChar = this.AddCardForm.value.CardNumber.charAt(0);
+    console.log(firstChar);
+    console.log("hejs");
 
   }
 
